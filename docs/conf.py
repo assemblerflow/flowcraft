da401361311b37e7ffbf0a1a21a2ac99cdf6f7e5
ODiogoSilva <o.diogosilva@gmail.com>
2018-02-22 15:13:09 +0000

    Added author

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'AssemblerFlow'
 copyright = '2018, Diogo N. Silva'
-author = 'Diogo N. Silva'
+author = 'Diogo N. Silva, Tiago F. Jesus'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
