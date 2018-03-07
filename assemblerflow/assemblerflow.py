af8527298a2fdc080d76fb5706d432c3946f3b62
ODiogoSilva <o.diogosilva@gmail.com>
2018-03-07 15:50:38 +0000

    Fixed PEP8 issues

--- assemblerflow/assemblerflow.py
+++ assemblerflow/assemblerflow.py
@@ -15,19 +15,15 @@ from distutils.dir_util import copy_tree
 from os.path import join, dirname
 
 try:
-    from generator import header_skeleton as hs
     from generator.engine import NextflowGenerator, process_map
     from generator.pipeline_parser import parse_pipeline, SanityError
     from generator.process_details import proc_collector, colored_print
-    import generator.process as pc
 except ImportError:
-    from assemblerflow.generator import header_skeleton as hs
     from assemblerflow.generator.engine import NextflowGenerator, process_map
     from assemblerflow.generator.pipeline_parser import parse_pipeline, \
         SanityError
     from assemblerflow.generator.process_details import proc_collector, \
         colored_print
-    import assemblerflow.generator.process as pc
 
 logger = logging.getLogger("main")
 
