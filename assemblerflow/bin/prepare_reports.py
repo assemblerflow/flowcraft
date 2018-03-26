1e9a4b5028197613a0be11f133af301325620d33
ODiogoSilva <o.diogosilva@gmail.com>
2018-03-26 16:40:19 +0100

    Bumped abricate version

--- assemblerflow/bin/prepare_reports.py
+++ assemblerflow/bin/prepare_reports.py
@@ -9,16 +9,20 @@ def write_json(report_json, task_name, project_name, sample_name, pid):
     with open(report_json) as fh:
         res = json.load(fh)
 
+    res["task"] = task_name
+
     report = {
         "report_json": res,
-        "task": task_name,
         "project_id": project_name,
         "sample_name": sample_name,
         "pipeline_id": sample_name,
-        "process_id": pid
+        "process_id": pid,
+        "user_id": 1,
+        "username": "user"
     }
 
-    with open("{}_{}_report.json".format(task_name, pid), "w") as report_fh:
+    with open("{}_{}_report.json".format(task_name, sample_name), "w") \
+            as report_fh:
         report_fh.write(json.dumps(report, separators=(",", ":")))
 
 
@@ -31,11 +35,17 @@ def main():
     project_name = args[3]
     pid = args[4]
 
-    write_json(report_json, task_name, project_name, sample_name, pid)
-
     print(report_json)
+    print(sample_name)
     print(task_name)
     print(project_name)
+    print(pid)
+
+    try:
+        write_json(report_json, task_name, project_name, sample_name, pid)
+    except json.decoder.JSONDecodeError:
+        print("Could not parse JSON output from {}, sample name {} and "
+              "pid {}".format(report_json, sample_name, pid))
 
 
 main()
