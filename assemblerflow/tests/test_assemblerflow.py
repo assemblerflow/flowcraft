639f870c595eac8fd27874055ca5df0a705d3de7
ODiogoSilva <o.diogosilva@gmail.com>
2018-04-08 13:19:38 +0100

    Fixed tests

--- assemblerflow/tests/test_assemblerflow.py
+++ assemblerflow/tests/test_assemblerflow.py
@@ -55,13 +55,9 @@ def test_build_file(tmp):
     p = os.path.join(os.path.abspath(tmp), "teste.nf")
     sys.argv.append(1)
 
-    args = af.get_args(["build", "-t integrity_coverage fastqc", "-o",
+    args = af.get_args(["build", "-t", "integrity_coverage fastqc", "-o",
                         "{}".format(p)])
     af.build(args)
-
-    assert sorted(os.listdir(tmp)) == ["containers.config", "lib",
-                                       "params.config", "resources.config",
-                                       "teste.html", "teste.nf", "user.config"]
 
 
 def test_build_file_2(tmp):
@@ -70,5 +66,9 @@ def test_build_file_2(tmp):
     p = os.path.join(os.path.abspath(tmp), "teste.nf")
 
     args = af.get_args(["build", "-t integrity_coverage fastqc", "-o",
-                        "{}".format(p), "--include-templates"])
+                        "{}".format(p), "--pipeline-only"])
     af.build(args)
+
+    assert sorted(os.listdir(tmp)) == ["containers.config", "lib",
+                                       "params.config", "resources.config",
+                                       "teste.html", "teste.nf", "user.config"]
