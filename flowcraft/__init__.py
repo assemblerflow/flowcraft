fe1740eced7121a6c50edcf3262dc5ed16acf358
ODiogoSilva <o.diogosilva@gmail.com>
2018-06-12 19:28:25 +0100

    Fixed non-ascii character in docs config

--- flowcraft/__init__.py
+++ flowcraft/__init__.py
@@ -1,7 +1,7 @@
 
 __version__ = "1.2.0"
 __build__ = "12052018"
-__author__ = "Diogo N. Silva, Tiago F. Jesus, Ines Mendes, Bruno Ribeiro-Gonçalves"
+__author__ = "Diogo N. Silva, Tiago F. Jesus, Ines Mendes, Bruno Ribeiro-Goncalves"
 __copyright__ = "Diogo N. Silva"
 __license__ = "GPL3"
 __maintainer__ = "Diogo N. Silva"
