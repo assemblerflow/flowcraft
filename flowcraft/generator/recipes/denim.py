3672a172054434558761642ea771daf8b3efb7d6
cimendes <17DEZembro11>
2018-09-25 14:41:26 +0100

    added DEN-IM recipe

--- flowcraft/generator/recipes/denim.py
+++ flowcraft/generator/recipes/denim.py
@@ -13,4 +13,31 @@ class Denim(Recipe):
 
         self.name = "denim"
 
-        self.pipeline_str = ""
+        self.pipeline_str = "integrity_coverage " \
+                            "fastqc_trimmomatic " \
+                            "filter_poly " \
+                            "remove_host " \
+                            "bowtie " \
+                            "retrieve_mapped " \
+                            "viral_assembly " \
+                            "assembly_mapping " \
+                            "pilon " \
+                            "split_assembly " \
+                            "(dengue_typing | mafft raxml)"
+
+        # Recipe parameters and directives
+        self.directives = {
+            "integrity_coverage": {
+                "params": {"genomeSize": "0.012", "minCoverage": "15"}
+            },
+            "bowtie":{
+                "directives": {"container": "flowcraft/bowtie_dengue", "version": "2-1"},
+                "pararm": {"reference": "'/ref/1_GenotypesDENV_14-05-18.fasta'"}
+            },
+            "assembly_mapping": {
+                "param": {"AMaxContigs": "1000", "genomeSize": "0.01"}
+            },
+            "split_assembly": {
+                "param": {"size": "10000"}
+            }
+        }
