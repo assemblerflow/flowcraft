197bb97b37e32a6b8ed1f13b2dd111c1f22f383f
cimendes <17DEZembro11>
2018-10-08 18:21:35 +0100

    added recipe for escmid's capacity building workshop for clinical metagenomics

---
+++ flowcraft/generator/recipes/metagenomics_workshop.py
@@ -0,0 +1,32 @@
+try:
+    from generator.recipe import Recipe
+except ImportError:
+    from flowcraft.generator.recipe import Recipe
+
+
+class Metagenomics_workshop(Recipe):
+    """
+    Recipe for ESCMID capacity building workshop 2018 - Clinical Microbiology
+    """
+
+    def __init__(self):
+
+        self.name = "metagenomics_workshop"
+
+        self.pipeline_str = "integrity_coverage " \
+                            "remove_host " \
+                            "fastqc_trimmomatic " \
+                            "(megahit " \
+                            "maxbin2 "\
+                            "(metaphlan_fa | " \
+                            "abricate | " \
+                            "mlst ) " \
+                            "| metaphlan_fq | " \
+                            "metamlst )"
+
+        # Recipe parameters and directives
+        self.directives = {
+            "abricate": {
+                "params": {"abricateDatabases": "resfinder"}
+            }
+        }
