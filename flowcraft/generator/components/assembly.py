4aa187d8ecb6f34a31f01d5cdf6fb98dd6473b76
eerhan <eerhan@sfu.ca>
2018-10-13 13:03:04 -0700

    Create abyss process class

--- flowcraft/generator/components/assembly.py
+++ flowcraft/generator/components/assembly.py
@@ -177,3 +177,39 @@ class ViralAssembly(Process):
                     "Use with caution."
             }
         }
+
+class Abyss(Process):
+    """ABySS process template interface
+
+    This process is set with:
+
+        - ``input_type``: fastq
+        - ``output_type``: assembly
+        - ``ptype``: assembly
+
+    """
+
+    def __init__(self, **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.input_type = "fastq"
+        self.output_type = "fasta"
+
+        # self.dependencies = ["integrity_coverage"]
+
+        self.params = {
+            "abyssKmer": {
+                "default": "96",
+                "description":
+                    "kmer size for assembly."
+            }
+        }
+
+        self.directives = {"abyss": {
+            "cpus": 4,
+            "memory": "{ 5.GB * task.attempt }",
+            "container": "flowcraft/abyss",
+            "version": "2.1.1",
+            "scratch": "true"
+        }}
